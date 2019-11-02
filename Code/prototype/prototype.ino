48fea0238048c89f753fc2a058a8ffd96c80a514
Sam C <sam.cung@carleton.ca>
2019-11-02 11:19:28 -0400

    Big update, added circuits, 3D components, added i2c prototype and modified code git push

--- Code/prototype/prototype.ino
+++ Code/prototype/prototype.ino
@@ -1,9 +1,9 @@
 #include <Servo.h>
+#include <SPI.h>
 
-Servo servo1;
-Servo servo2;
-Servo selectedServo;
-char moduleOneArray[2] = {'1','1'};
+
+Servo servo[2];
+char moduleOneArray[3] = {'0', '0', '0'};
 
 const byte numChars = 32;
 char receivedChars[2];
@@ -14,51 +14,46 @@ boolean newData = false;
 
 void setup() {
   // put your setup code here, to run once:
-  servo1.attach(5);
-  servo1.write(0);
-
-  servo2.attach(4); 
-  servo2.write(0);
-  
-  delay(2000);
+  configureServos();
+  Serial.begin(9600);
+  delay(500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int j = 0; j < 10; j++){
-    for(int i = 1; i < 3; i++){
-      switch(i){
-        case 1:
-          selectedServo = servo1;
-          break;
-        case 2:
-          selectedServo = servo2;
-          break;
-        default:
-          break;
-      }
-      switch(moduleOneArray[i-1]){
-        case '1':
-          selectedServo.write(180);
-          delay(500);
-          selectedServo.write(0);
-          break;
-        default:
-          break;
-      }
+//  for(int i = 1; i < 3; i++){
+//    switch(i){
+//      case 1:
+//        selectedServo = servo1;
+//        break;
+//      case 2:
+//        selectedServo = servo2;
+//        break;
+//      default:
+//        break;
+//    }//switch
+//    switch(moduleOneArray[i-1]){
+//      case '1':
+//        selectedServo.write(180);
+//        delay(500);
+//        selectedServo.write(0);
+//        break;
+//      default:
+//        break;
+//    }//switch
+//  }//for i
+
   
-      
-    }
+
+
+
+//  servo1.detach();
+//  servo2.detach();
+}
+
+void configureServos(){
+  for(int i = 4; i < 6; i++){
+    servo[i].attach(i);
+    servo[i].write(0);
   }
-  //delay(500);
-  for(int k = 0; k < 30; k++){
-    servo1.write(90);
-    servo2.write(90);
-    delay(100);
-    servo1.write(180);
-    servo2.write(180);
-    delay(100);
-  }
-  servo1.detach();
-  servo2.detach();
 }
