636640e1c7e23af73c3a0f0be9fec06075b59200
Sam C <sam.cung@carleton.ca>
2019-11-05 19:47:29 -0500

    finished i2c connection of Rpi to Arduino

--- Code/I2C connection/testing_I2C/testing_I2C.ino
+++ Code/I2C connection/testing_I2C/testing_I2C.ino
@@ -1,11 +1,57 @@
+#include <Wire.h>
+#include <Servo.h>
 
+#define SLAVE_ADDRESS 0x04
+int number = 0;
+int state = 0;
+Servo servo1;
 
 void setup() {
-  // put your setup code here, to run once:
+  servo1.attach(4);
   
+  Serial.begin(9600); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+  
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+  servo1.write(180);
+  delay(500);
+  servo1.write(0);
+  Serial.println("Ready!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(100);
+}
 
+// callback for received data
+void receiveData(int byteCount){
+
+  while(Wire.available()) {
+    number = Wire.read();
+    Serial.print("data received: ");
+    Serial.println(number);
+    
+    if (number == 1){  
+      servo1.write(180);
+      delay(500);
+    }
+
+    if(number == 0){
+      servo1.write(0);
+      delay(500);
+    }
+
+    if(number == 2){
+      servo1.detach();
+    }
+  }
+}
+
+// callback for sending data
+void sendData(){
+  Wire.write(number);
 }
